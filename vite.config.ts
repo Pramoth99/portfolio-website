529945e4b00c65f78eb25a42fd8836cb45d154eb
Pramoth99 <pvmadhav@uwaterloo.ca>
2025-04-07 12:15:00 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/Pramoth99/Portfolio/",
+  base:"/Portfolio/",
   plugins: [react()],
 })
